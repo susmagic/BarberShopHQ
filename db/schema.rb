5f990c9548732c43955c58a30ba5bbd23fcbafad
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-01-10 19:45:51 +0300

    update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160110160638) do
     t.datetime "updated_at"
   end
 
-  create_table "client", force: :cascade do |t|
+  create_table "clients", force: :cascade do |t|
     t.text     "name"
     t.text     "phone"
     t.text     "datestamp"
