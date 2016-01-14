8ab5b9de2bf84979012513f013b45aa2ccb35019
Ruslan <ruslan.fekhretdinov@gmail.com>
2016-01-14 22:46:33 +0300

    display error on failed validations

--- app.rb
+++ app.rb
@@ -36,10 +36,11 @@ end
 
 post '/visit' do
     c = Client.new params[:client]
-    c.save
-
-    erb "<h2>Спасибо, Вы записались!</h2>"
-
+    if c.save
+        erb "<h2>Спасибо, Вы записались!</h2>"
+    else
+        erb "<h2>ошибочка</h2>"
+    end
 end
 
 post '/contacts' do
